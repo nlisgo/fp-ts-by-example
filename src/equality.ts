744ed0d9ff4a3361e0d85c19fe6aa69f8dcf5671
Nathan Lisgo <nathan@lisgo.co.uk>
2025-10-15 07:12:01 +0100

    Refactor equality checks with updated fp-ts imports and examples

--- src/equality.ts
+++ src/equality.ts
@@ -1,9 +1,14 @@
-import {
-  boolean, date, number, string,
-} from 'fp-ts';
+import * as D from 'fp-ts/Date';
+import * as B from 'fp-ts/boolean';
 import { pipe } from 'fp-ts/function';
+import * as N from 'fp-ts/number';
+import * as S from 'fp-ts/string';
 
-pipe(boolean.Eq.equals(true, true), console.log); // true
-date.Eq.equals(new Date('1984-01-27'), new Date('1984-01-27')); // true
-number.Eq.equals(3, 3); // true
-string.Eq.equals('Cyndi', 'Cyndi'); // true
+pipe(B.Eq.equals(false, true), console.log); // false
+pipe(B.Eq.equals(true, true), console.log); // true
+pipe(D.Eq.equals(new Date('1984-01-27'), new Date('1984-01-28')), console.log); // false
+pipe(D.Eq.equals(new Date('1984-01-27'), new Date('1984-01-27')), console.log); // true
+pipe(N.Eq.equals(2, 3), console.log); // false
+pipe(N.Eq.equals(3, 3), console.log); // true
+pipe(S.Eq.equals('Cindi', 'Cyndi'), console.log); // false
+pipe(S.Eq.equals('Cyndi', 'Cyndi'), console.log); // true
