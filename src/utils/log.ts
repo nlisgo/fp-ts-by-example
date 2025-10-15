23a9253563c75712284f210189d2cfda60b063f6
Nathan Lisgo <nathan@lisgo.co.uk>
2025-10-15 18:13:24 +0100

    Reorder import statements to comply with eslint rules

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,6 +1,6 @@
-// eslint-disable-next-line no-console
 import { pipe } from 'fp-ts/function';
 
+// eslint-disable-next-line no-console
 export const log = <A, B>(m: A) => (v: B): void => console.log(m, v);
 
 export const pipeAndLog = <A, B>(p: A, m: B): void => pipe(
