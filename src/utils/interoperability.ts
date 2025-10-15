dab97913ceac2eb0621abe2331451aab953f3803
Nathan Lisgo <nathan@lisgo.co.uk>
2025-10-15 18:39:34 +0100

    Add utility functions for interoperability using fp-ts constructs

---
+++ src/utils/interoperability.ts
@@ -0,0 +1,29 @@
+import * as E from 'fp-ts/Either';
+import * as IO from 'fp-ts/IO';
+import * as O from 'fp-ts/Option';
+import * as T from 'fp-ts/Task';
+import * as TE from 'fp-ts/TaskEither';
+
+export const findIndex = <A>(as: Array<A>, predicate: (a: A) => boolean): O.Option<number> => {
+  const index = as.findIndex(predicate);
+  return index === -1 ? O.none : O.some(index);
+};
+
+export const find = <A>(as: Array<A>, predicate: (a: A) => boolean): O.Option<A> => O.fromNullable(as.find(predicate));
+
+export const parse = (s: string): E.Either<Error, unknown> => E.tryCatch(
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+  () => JSON.parse(s),
+  (reason) => new Error(String(reason)),
+);
+
+export const random: IO.IO<number> = () => Math.random();
+
+export const wait: T.Task<void> = async () => new Promise<void>((resolve) => {
+  setTimeout(resolve, 1000);
+});
+
+export const get = (url: string): TE.TaskEither<Error, string> => TE.tryCatch(
+  async () => fetch(url).then(async (res) => res.text()),
+  (reason) => new Error(String(reason)),
+);
