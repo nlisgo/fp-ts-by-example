fb3bea036864a6b54898b0dc353a97247208a8c5
Nathan Lisgo <nathan@lisgo.co.uk>
2025-10-17 13:07:05 +0100

    Add examples demonstrating usage of fp-ts Option and pipe functions

---
+++ src/practical-guide-to-fp-ts/part-2.ts
@@ -0,0 +1,71 @@
+import * as O from 'fp-ts/Option';
+import { pipe } from 'fp-ts/function';
+import { pipeAndLog } from '../utils/log';
+
+{
+  const foo = {
+    bar: 'hello',
+  };
+
+  pipeAndLog(
+    pipe(
+      foo,
+      (f) => f.bar,
+    ),
+    1,
+  ); // hello
+}
+
+{
+  type Foo = {
+    bar: string,
+  };
+
+  const foo = {
+    bar: 'hello',
+  } as Foo | undefined;
+
+  pipeAndLog(
+    pipe(
+      foo,
+      (f) => f?.bar,
+    ),
+    2,
+  ); // hello
+
+  pipeAndLog(
+    pipe(
+      foo,
+      O.fromNullable,
+    ),
+    3.1,
+  ); // { _tag: 'Some', value: 'hello' }
+
+  pipeAndLog(
+    pipe(
+      undefined,
+      O.fromNullable,
+    ),
+    3.2,
+  ); // { _tag: 'None' }
+}
+
+{
+  type Fizz = {
+    buzz: string,
+  };
+
+  type Foo = {
+    bar?: Fizz,
+  };
+
+  const foo = { bar: undefined } as Foo | undefined;
+
+  pipeAndLog(
+    pipe(
+      foo,
+      (f) => f?.bar?.buzz,
+    ),
+    4.1,
+  ); // undefined
+}
